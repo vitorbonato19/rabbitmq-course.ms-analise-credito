622f1ee284f63be7e7698bf28bcaa82b8330d2fe
Vitor Bonato <vitorbonatocam@gmail.com>
2024-07-08 20:07:10 -0300

    first commit

---
+++ src/main/java/com/udemy/analisecredito/AnalisecreditoApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.analisecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisecreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisecreditoApplication.class, args);
+	}
+
+}
